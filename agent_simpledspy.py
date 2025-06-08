7ec703d7ac95ec74f2eb6f9b258c0a32e3349777
Tom Dörr <tomdoerr96@gmail.com>
2025-06-08 16:52:25 +0200

    .

--- agent_simpledspy.py
+++ agent_simpledspy.py
@@ -3,11 +3,15 @@
 
 from simpledspy import predict, chain_of_thought
 import os
-from subprocess import run, PIPE
+from subprocess import run, PIPE, STDOUT, Popen
 from typing import Literal
 import rich
 import dspy
-dspy.configure(lm=dspy.LM('openrouter/google/gemini-2.5-flash-preview'))
+import sys
+
+# dspy.configure(lm=dspy.LM('openrouter/google/gemini-2.5-flash-preview'))
+dspy.configure(lm=dspy.LM('deepseek/deepseek-reasoner', max_tokens=20000))
+
 #dspy.configure(lm=dspy.LM('openrouter/deepseek/deepseek-r1-0528'))
 
 
@@ -40,13 +44,22 @@ class Agent(dspy.Module):
                 command = chain_of_thought(self.context, description='Please generate a shell command for the command variable for running with subprocess.run. The command should be a string that can be executed in the shell.')
                 self.context += f'Command: {command}\n'
                 print(f'Running command: {command}')
-                result = run(command, shell=True, stdout=PIPE, stderr=PIPE, text=True)
-                if result.returncode == 0:
-                    print(f'Command output: {result.stdout}')
-                    self.context += f'Command output: {result.stdout}\n'
-                else:
-                    print(f'Command error: {result.stderr}')
-                    self.context += f'Command error: {result.stderr}\n'
+                # result = run(command, shell=True, stdout=PIPE, stderr=PIPE, text=True)
+                process = Popen(command, shell=True, stdout=PIPE, stderr=STDOUT, text=True, bufsize=1)
+                self.context += f'Process started with PID: {process.pid}\n'
+                self.context += f'Command output:\n'
+                for line in process.stdout:
+                    print(line, end='')
+                    self.context += f'{line}'
+
+                process.wait()
+                self.context += f'Process finished with return code: {process.returncode}\n'
+#                if result.returncode == 0:
+#                    print(f'Command output: {result.stdout}')
+#                    self.context += f'Command output: {result.stdout}\n'
+#                else:
+#                    print(f'Command error: {result.stderr}')
+#                    self.context += f'Command error: {result.stderr}\n'
             elif action == 'reply_to_user':
                 response = predict(self.context)
                 self.context += f'Response: {response}\n'
@@ -56,7 +69,7 @@ class Agent(dspy.Module):
                 print(f'Unknown action: {action}')
                 self.context += f'Unknown action: {action}\n'
             options = ['True', 'False']
-            done = predict(self.context, options, description='Is the agent done for now or does it need to continue working to finish open tasks? Output True or False. Reply True if we need to wait for the user to reply, False if we can continue working.')
+            done = predict(self.context, options, description='Is the agent done for now or does it need to continue working to finish open tasks? Output True or False. Reply True if we need to wait for the user to reply, False if we can continue working. Only output True if you are sure that the agent is done and no further actions are needed.')
             self.context += f'Done: {done}\n'
             #self.print_context()
             if done == 'True':
@@ -68,55 +81,17 @@ class Agent(dspy.Module):
                 self.context += f'Unknown option: {done}\n'
 
 
+
+
 agent = Agent()
+
+if len(sys.argv) > 1 and '--' in sys.argv:
+    # if we have a command line argument, use it as the initial user input
+    initial_input = ' '.join(sys.argv[1:]).replace('--', '')
+    print(f'Initial input: {initial_input}')
+    agent.forward(initial_input)
 while True:
     user_input = input('> ')
     agent.forward(user_input)
-
-
-input()
-context = ''
-while True:
-    user_input = input('> ')
-    context += f'\nUser: {user_input}\n'
-#    response = predict(user_input)
-#    print(response)
-    available_actions = ['run_command', 'reply_to_user']
-    while True:
-        #action = predict(context, available_actions, description='Choose an action for the agent to perform') 
-        context += f'\nAgent action: {action}\n'
-        if action == 'run_command':
-            command = predict(context)
-            context += f'\nCommand: {command}\n'
-            print(f'Running command: {command}')
-            result = run(command, shell=True, stdout=PIPE, stderr=PIPE, text=True)
-            if result.returncode == 0:
-                print(f'Command output: {result.stdout}')
-                context += f'\nCommand output: {result.stdout}\n'
-            else:
-                print(f'Command error: {result.stderr}')
-                context += f'\nCommand error: {result.stderr}\n'
-        elif action == 'reply_to_user':
-            response = predict(context)
-            context += f'\nResponse: {response}\n'
-            print(f'Agent response: {response}')
-        else:
-            print(f'Unknown action: {action}')
-            context += f'\nUnknown action: {action}\n'
-
-
-        options = ['True', 'False']
-        done = predict(context, options)
-        context += f'\nDone: {done}\n'
-        if done == 'True':
-            break
-        elif done == 'False':
-            continue
-        else:
-            print(f'Unknown option: {done}')
-            context += f'\nUnknown option: {done}\n'
-
-
-
 
 
--- simpledspy_main.py
+++ simpledspy_main.py
@@ -1,12 +1,33 @@
 #!/usr/bin/env python3
 
 from simpledspy import predict
+import dspy
+
+# dspy.configure(lm=dspy.LM('openrouter/google/gemini-2.5-flash-preview', cache=False, max_tokens=10000))
+# dspy.configure(lm=dspy.LM('deepseek/deepseek-chat'))
+dspy.configure(lm=dspy.LM('deepseek/deepseek-reasoner', cache=False, max_tokens=10000))
+
 
 a = 'jkl'
 b = 'abc'
+c = 'xyz'
+
+a_reversed, b_repeated, a_plus_b, c_plus_a = predict(b, a, c)
+print(f'a_reversed: {a_reversed}'); print(f'b_repeated: {b_repeated}'); print(f'a_plus_b: {a_plus_b}'); print(f'c_plus_a: {c_plus_a}')
+
+a_reversed, b_repeated, a_plus_b = predict(b, a)
+print(f'a_reversed: {a_reversed}'); print(f'b_repeated: {b_repeated}'); print(f'a_plus_b: {a_plus_b}')
+
 
 # a_reversed, b_repeated = predict(b, a)
-a_reversed, b_repeated = predict(b, a, description='reverse a and repeat b')
-print(f'a_reversed: {a_reversed}')
-print(f'b_repeated: {b_repeated}')
+# a_reversed, b_repeated = predict(b, a, description='reverse a and repeat b')
+a_reversed, b_repeated = predict(b, a)
+print(f'a_reversed: {a_reversed}'); print(f'b_repeated: {b_repeated}')
 
+# a_reversed, b_repeated = predict(a, b, description='reverse a and repeat b')
+a_reversed, b_repeated = predict(a, b)
+print(f'a_reversed: {a_reversed}'); print(f'b_repeated: {b_repeated}')
+
+# b_reversed, a_repeated = predict(a, b, description='reverse a and repeat b')
+b_reversed, a_repeated = predict(a, b)
+print(f'a_reversed: {a_reversed}'); print(f'b_repeated: {b_repeated}')
