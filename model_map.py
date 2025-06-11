530753c23c7d2106b86d4323eb54e4f0620d0fa8
Tom Dörr <tomdoerr96@gmail.com>
2025-06-11 22:48:24 +0200

    .

--- model_map.py
+++ model_map.py
@@ -1,8 +1,8 @@
 MODEL_MAP = {
         'flash': 'openrouter/google/gemini-2.5-flash-preview',
         'r1': 'openrouter/deepseek/deepseek-r1-0528',
+        'dv3': 'openrouter/deepseek/deepseek-chat-v3-0324',
         'd/r1': 'deepseek/deepseek-reasoner',
         'd/v3': 'deepseek/deepseek-chat',
-        'dv3': 'openrouter/deepseek/deepseek-chat-v3-0324',
 }
 
